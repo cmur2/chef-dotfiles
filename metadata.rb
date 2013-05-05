2df44bf0e6eeaadafaecb616e1d724626c83bb38
cn <cn@mycrobase.de>
2013-05-05 11:29:09 +0200

    Add desc

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "dotfiles"
 maintainer       "Christian Nicolai"
 maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
-description      ""
+description      "This cookbook clones a git repository per specified user and afterwards calls a configuration script."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
