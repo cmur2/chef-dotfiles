c2f9287375e0767bc445f78ddb7e8a45e2142091
cn <cn@mycrobase.de>
2013-05-07 20:47:05 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "This cookbook clones a git repository per specified user and afterwards calls a configuration script."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 
 depends "git"
 
